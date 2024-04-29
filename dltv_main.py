c47a514bbacc06df01b43a0812a507b00fb56c93
Kamil' Sitdikov <believer89793@yandex.com>
2024-04-29 11:00:11 +0300

    bug fix

--- dltv_main.py
+++ dltv_main.py
@@ -2,6 +2,19 @@ import time
 import traceback
 import dltv_cyberscore
 while True:
-    dltv_cyberscore.get_live_matches()
-    print('Сплю 3 минут')
-    time.sleep(180)
+    try:
+        dltv_cyberscore.get_live_matches()
+    except Exception as e:
+        error_message = str(e)
+        # Получаем последний объект трассировки и извлекаем номер строки
+        tb = traceback.TracebackException.from_exception(e)
+        frame = tb.stack[-1]
+        filename = frame.filename
+        line_no = frame.lineno
+
+        error_info = f"Ошибка: {error_message} в файле {filename}, строка {line_no}"
+
+        with open('errors.txt', 'r+') as f:
+            f.write(error_info)
+    print('Сплю 2 минуты')
+    time.sleep(120)

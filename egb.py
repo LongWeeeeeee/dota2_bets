acb94b119237acea0607610d40160ae83c166bea
Kamil' Sitdikov <believer89793@yandex.com>
2024-05-27 23:20:31 +0300

    bug figx

--- egb.py
+++ egb.py
@@ -333,7 +333,7 @@ while True:
                         answer = get_picks_and_pos(exac_match)
                         if answer is not None:
                             radiant, dire, match_id = answer
-                            dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, only_good_bets=True, tier=2)
+                            dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, tier=2)
                     else:
                         print('карта не найдена, вероятно, матч только начался')
     except: pass

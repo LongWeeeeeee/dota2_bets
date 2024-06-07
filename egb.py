ebf094b6ffb1c0747f56d07f2855d97d601494f2
Kamil' Sitdikov <believer89793@yandex.com>
2024-06-07 12:58:31 +0300

    bug figx

--- egb.py
+++ egb.py
@@ -419,29 +419,29 @@ def get_picks_and_pos(match_id):
         print('карта уже расчитана')
 
 
-# while True:
-#     try:
-#         importlib.reload(id_to_name)
-#         response = requests.get(url, params=params, headers=headers)
-#         if response.status_code == 200:
-#             data = json.loads(response.text)
-#             for bet in data['bets']:
-#                 if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
-#                     players_ids, dire_and_radiant = get_players(bet)
-#                     response = get_strats_graph_match()
-#                     exac_match = get_exac_match(response, players_ids)
-#                     if exac_match is not None:
-#                         answer = get_picks_and_pos(match_id=exac_match['matchId'])
-#                         if answer is not None:
-#                             radiant, dire, match_id = answer
-#                             print(f'Radint pick: {radiant}\nDire pick: {dire}')
-#                             dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, tier=2)
-#                     else:
-#                         print('карта не найдена, вероятно, матч только начался')
-#     except: pass
-#     print('сплю 3 минуты')
-#     time.sleep(160)
-answer = get_picks_and_pos(match_id=7781964303)
-if answer is not None:
-    radiant, dire, match_id = answer
-    dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name='radiant', dire_team_name='dire', antiplagiat_url=match_id, tier=2)
+while True:
+    try:
+        importlib.reload(id_to_name)
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            for bet in data['bets']:
+                if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
+                    players_ids, dire_and_radiant = get_players(bet)
+                    response = get_strats_graph_match()
+                    exac_match = get_exac_match(response, players_ids)
+                    if exac_match is not None:
+                        answer = get_picks_and_pos(match_id=exac_match['matchId'])
+                        if answer is not None:
+                            radiant, dire, match_id = answer
+                            print(f'Radint pick: {radiant}\nDire pick: {dire}')
+                            dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, tier=2)
+                    else:
+                        print('карта не найдена, вероятно, матч только начался')
+    except: pass
+    print('сплю 3 минуты')
+    time.sleep(160)
+# answer = get_picks_and_pos(match_id=7781964303)
+# if answer is not None:
+#     radiant, dire, match_id = answer
+#     dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name='radiant', dire_team_name='dire', antiplagiat_url=match_id, tier=2)

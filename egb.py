fa09e25fb8a78be8fb4bac51bfe53192933216b5
Kamil' Sitdikov <believer89793@yandex.com>
2024-06-11 16:19:49 +0300

    bug fix

--- egb.py
+++ egb.py
@@ -6,7 +6,22 @@ import time
 from dltv_cyberscore import dota2protracker, if_unique, send_message
 import requests
 
+url = "https://egb.com/bets"
+params = {
+    "active": "true",
+    "st": "1714418601",
+    "ut": "1714418584"
+}
 
+headers = {
+    "Host": "egb.com",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "Accept": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Content-Type": "application/json",
+    "X-CSRF-Token": "UUu1IL8sY0iZFFc2_FYI4ICk-WT34IRRGjz19DND8CbKEJZ9zTvbeAdcw72OYEecZiDlBaZimaYP-VuJwtmkAQ",
+    "DNT": "1",
+    "Sec-GPC": "1",}
 
 def get_players(bet):
     dire_and_radiant = {}
@@ -519,32 +534,30 @@ def get_picks_and_pos(match_id):
 
 while True:
     map = False
-    try:
-        importlib.reload(id_to_name)
-        response = requests.get(url, params=params, headers=headers)
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            for bet in data['bets']:
-                if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
-                    answer = get_players(bet)
-                    if answer is not None:
-                        if answer != True:
-                            players_ids, dire_and_radiant = answer
-                            response = get_strats_graph_match()
-                            exac_match = get_exac_match(response, players_ids)
-                            if exac_match is not None:
-                                answer = get_picks_and_pos(match_id=exac_match['matchId'])
-                                if answer is not None:
-                                    if answer != True:
-                                        radiant, dire, match_id, output_message = answer
-                                        print(f'Radint pick: {radiant}\nDire pick: {dire}')
-                                        dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, score = [0,0], egb=True, output_message=output_message)
-                                    else:
-                                        map = True
-                            else:
-                                print('карта не найдена, вероятно, матч только начался')
-                                map = True
-    except: pass
+    importlib.reload(id_to_name)
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        for bet in data['bets']:
+            if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
+                answer = get_players(bet)
+                if answer is not None:
+                    if answer != True:
+                        players_ids, dire_and_radiant = answer
+                        response = get_strats_graph_match()
+                        exac_match = get_exac_match(response, players_ids)
+                        if exac_match is not None:
+                            answer = get_picks_and_pos(match_id=exac_match['matchId'])
+                            if answer is not None:
+                                if answer != True:
+                                    radiant, dire, match_id, output_message = answer
+                                    print(f'Radint pick: {radiant}\nDire pick: {dire}')
+                                    dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, score = [0,0], egb=True, output_message=output_message)
+                                else:
+                                    map = True
+                        else:
+                            print('карта не найдена, вероятно, матч только начался')
+                            map = True
     if map:
         print('сплю 30 секунд')
         time.sleep(30)

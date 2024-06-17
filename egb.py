5aa49e14c199e256876ecebdf0c2f761d059f298
Kamil' Sitdikov <believer89793@yandex.com>
2024-06-17 16:43:33 +0300

    bug fix

--- egb.py
+++ egb.py
@@ -634,41 +634,41 @@ def check_players_skill(radiant, dire, output_message, R_pos_strng, D_pos_strng)
 
 
 
-while True:
-    map = False
-    importlib.reload(id_to_name)
-    response = requests.get(url, params=params, headers=headers)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        for bet in data['bets']:
-            if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
-                answer = get_players(bet)
-                if answer is not None:
-                    if answer != True:
-                        players_ids, dire_and_radiant = answer
-                        response = get_strats_graph_match()
-                        exac_match = get_exac_match(response, players_ids)
-                        if exac_match is not None:
-                            answer = get_picks_and_pos(match_id=exac_match['matchId'])
-                            if answer is not None:
-                                if answer != True:
-                                    radiant, dire, match_id, output_message, R_pos_strng, D_pos_strng = answer
-                                    output_message, impact_diff, R_pos_strng, D_pos_strng, player_check = check_players_skill(radiant, dire, output_message, R_pos_strng, D_pos_strng)
-                                    output = ", ".join([f"'{pos}' : '{data['hero_name']}'" for pos, data in radiant.items()])
-                                    dire_output= ", ".join([f"'{pos}' : '{data['hero_name']}'" for pos, data in dire.items()])
-                                    print(f'Radint pick: {output}\nDire pick: {dire_output}')
-                                    dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, score = [0,0], egb=True, output_message=output_message, impact_diff=impact_diff, R_pos_strng=R_pos_strng, D_pos_strng=D_pos_strng, player_check=player_check)
-                                else:
-                                    map = True
-                        else:
-                            print('карта не найдена, вероятно, матч только начался')
-                            map = True
-    if map:
-        print('сплю 30 секунд')
-        time.sleep(30)
-    else:
-        print('сплю 3 минуты')
-        time.sleep(160)
+# while True:
+#     map = False
+#     importlib.reload(id_to_name)
+#     response = requests.get(url, params=params, headers=headers)
+#     if response.status_code == 200:
+#         data = json.loads(response.text)
+#         for bet in data['bets']:
+#             if bet['esports'] and bet['streams_enabled'] and bet['game'] == 'Dota 2' and bet['tourn'] in ['Incubator','Ladder Games']:
+#                 answer = get_players(bet)
+#                 if answer is not None:
+#                     if answer != True:
+#                         players_ids, dire_and_radiant = answer
+#                         response = get_strats_graph_match()
+#                         exac_match = get_exac_match(response, players_ids)
+#                         if exac_match is not None:
+#                             answer = get_picks_and_pos(match_id=exac_match['matchId'])
+#                             if answer is not None:
+#                                 if answer != True:
+#                                     radiant, dire, match_id, output_message, R_pos_strng, D_pos_strng = answer
+#                                     output_message, impact_diff, R_pos_strng, D_pos_strng, player_check = check_players_skill(radiant, dire, output_message, R_pos_strng, D_pos_strng)
+#                                     output = ", ".join([f"'{pos}' : '{data['hero_name']}'" for pos, data in radiant.items()])
+#                                     dire_output= ", ".join([f"'{pos}' : '{data['hero_name']}'" for pos, data in dire.items()])
+#                                     print(f'Radint pick: {output}\nDire pick: {dire_output}')
+#                                     dota2protracker(radiant_heroes_and_positions=radiant, dire_heroes_and_positions=dire, radiant_team_name=dire_and_radiant['radiant'], dire_team_name=dire_and_radiant['dire'], antiplagiat_url=match_id, score = [0,0], egb=True, output_message=output_message, impact_diff=impact_diff, R_pos_strng=R_pos_strng, D_pos_strng=D_pos_strng, player_check=player_check)
+#                                 else:
+#                                     map = True
+#                         else:
+#                             print('карта не найдена, вероятно, матч только начался')
+#                             map = True
+#     if map:
+#         print('сплю 30 секунд')
+#         time.sleep(30)
+#     else:
+#         print('сплю 3 минуты')
+#         time.sleep(160)
 # answer = get_picks_and_pos(match_id=7781964303)
 # if answer is not None:
 #     radiant, dire, match_id = answer
